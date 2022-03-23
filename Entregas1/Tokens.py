015ffa10bf81d7fea6cad793927a183d7a09e3d8
milenatteixeira <milenateixeira648@gmail.com>
2022-03-23 19:59:18 -0300

    added tokens file

---
+++ Entregas1/Tokens.py
@@ -0,0 +1,21 @@
+#### Compilador
+
+## Integrantes
+# Marcella Costa
+# Mateus Queiroz
+# Milena Teixeira
+# Yasmin Vitorino
+
+## Palavras-chave
+# if        |       | ^()$
+# elif      |       | ^()$
+# else      |       | ^()$
+# for       |    | ^()$
+# while     |      | ^()$
+# functions | comeback   | ^()$
+# return    | kamsamida  | ^()$
+# print     | annyeong   | ^()$
+# int       | yg         | ^()$
+# float     | jyp        | ^()$
+# bool      | sm         | ^()$
+# string    | hybe       | ^()$
