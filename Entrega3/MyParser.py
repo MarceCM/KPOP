01ce71bc4f8cc2fc6685cbef117d4eb5e68a0ed3
unifmfigueiredo <79601812+unifmfigueiredo@users.noreply.github.com>
2022-05-11 22:27:11 -0300

    atr

--- Entrega3/MyParser.py
+++ Entrega3/MyParser.py
@@ -92,19 +92,13 @@ class Interpreter(object):
       self.eat(BIAS)
       return token.value
 
-  #def atr(self):
-  #token = self.current_token
-    #if token.type == BIAS:
-      #self.eat(BIAS)
-      #return token.value
-      #self.eat(MELON)
-      #self.value()
-  #def atr(self):
-  #"iden MELON value (SULJIBN sijag)*"
-    #self.iden() = self.value()
-    #while self.current_token == SULJIBN:
-      #self.eat(SULJIBN)
-      #result += self.sijag()
+  
+  def atr(self):
+  "iden MELON value (SULJIBN sijag)*"
+    self.iden() = self.value()
+    while self.current_token == SULJIBN:
+      self.eat(SULJIBN)
+      result += self.sijag()
 
     return result 
     
